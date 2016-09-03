270a74a6e8daabaa29e25b96d3a0634057295a3b
dani2000 <daniel.renaud@divcom.ch>
2016-09-03 16:52:39 +0200

    changement direction

--- src/Directions.java
+++ src/Directions.java
@@ -5,18 +5,21 @@ import lejos.hardware.sensor.SensorMode;
 
 public class Directions {
 
-	public void direction(EV3LargeRegulatedMotor moteurDroit, EV3LargeRegulatedMotor moteurGauche, float[] valeurCouleur, EV3MediumRegulatedMotor bras) {
+	public void direction(EV3LargeRegulatedMotor moteurDroit, EV3LargeRegulatedMotor moteurGauche,
+			float[] valeurCouleur, EV3MediumRegulatedMotor bras) {
 
-		if (valeurCouleur[1] >= valeurCouleur[0]-5 && valeurCouleur[1] <= valeurCouleur[0]+5){
-			moteurDroit.forward();
+		if (valeurCouleur[1] >= valeurCouleur[0] - 5 && valeurCouleur[1] <= valeurCouleur[0] + 5) {
+
+			bras.rotate(90);
+
 			moteurDroit.setSpeed(50);
 			moteurDroit.rotate(30);
-			moteurDroit.stop();
-			
-			
-		}
-		else {
-			
+
+			moteurDroit.setSpeed(50);
+			moteurDroit.rotate(-60);
+
+		} else {
+
 		}
 
 	}
