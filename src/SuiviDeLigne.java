90dbded6bcbb3b4ece3950ebd36b64f68c727a94
Astreptocoque <astreptocoque@gmail.com>
2016-09-03 16:54:44 +0200

    alkdfdjlsfk

--- src/SuiviDeLigne.java
+++ src/SuiviDeLigne.java
@@ -8,14 +8,33 @@ public class SuiviDeLigne {
 
 		moteurDroit.forward();
 		moteurGauche.forward();
+		
+		moteurDroit.setSpeed(100);
+		moteurGauche.setSpeed(100);
+		
+		boolean sortDeLaBoucle = true;
+		float[] sampleBloc = new float[couleurBloc.sampleSize()];
+		float[] sampleLigne = new float[couleurBloc.sampleSize()];
 
-		while (true) {
-			float[] sample = null;
-			couleurLigne.fetchSample(sample, 0);
-			float positionligne = sample[0] - 0.09f;
-			float correction = positionligne * 3000f;
+		
+		float moyenneLumiere = 0;
+		float correctionbla = 0;
+
+		
+		while (sortDeLaBoucle){
+			couleurLigne.fetchSample(sampleLigne, 0);
+			float positionligne = sampleLigne[0] - moyenneLumiere;
+			float correction = positionligne * correctionbla;
 			float vitessegauche = 130 + correction;
 			float vitessedroite = 130 - correction;
+			moteurDroit.setSpeed(vitessedroite);
+			moteurGauche.setSpeed(vitessegauche);
+			
+			/// détecte le bloc
+			couleurBloc.fetchSample(sampleBloc, 0);
+			if(sampleBloc[0] <= 90){
+				sortDeLaBoucle = false;
+			}
 
 		}
 
